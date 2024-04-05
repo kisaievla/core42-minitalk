45eb4236997dd873f369dbd13e14de3ead5eb9c7
Vlad Isaienko <kisaievla@icloud.com>
2024-04-05 19:57:15 +0200

    Continues making client. Need to watch the video about sigaction

--- ft_client.c
+++ ft_client.c
@@ -6,7 +6,7 @@
 /*   By: visaienk <visaienk@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 17:34:11 by visaienk          #+#    #+#             */
-/*   Updated: 2024/04/05 19:18:12 by visaienk         ###   ########.fr       */
+/*   Updated: 2024/04/05 19:56:30 by visaienk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,8 @@ int	main(int argc, char **argv)
 	if (argc != 3)
 		ft_write("Some args are missing");
 	else
-		ft_write(argv[argc - 1]);
+	{
+		typedef struct sigaction s_sa;
+		s_sa.
+	}
 }
